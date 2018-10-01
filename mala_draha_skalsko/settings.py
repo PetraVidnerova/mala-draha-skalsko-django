db61abd3dbe89e152f30117c623a3d1e3a692fa1
Petra Vidnerova <petra.vidnerova@gmail.com>
2018-10-01 16:57:54 +0200

    secret key in file

--- mala_draha_skalsko/settings.py
+++ mala_draha_skalsko/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5za#em=o7__sbqkvpds@j%-wa^plbc(uq^23n*xoeg9zhfnd=5'
+with open("key.txt") as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
